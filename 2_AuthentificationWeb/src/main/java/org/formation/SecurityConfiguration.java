84ae5d7136716e19a843e81d6eac13d9e95c5322
David THIBAU <david.thibau@gmail.com>
2023-02-19 12:54:14 +0100

    ACLs Bump to 3.3

--- 2_AuthentificationWeb/src/main/java/org/formation/SecurityConfiguration.java
+++ 2_AuthentificationWeb/src/main/java/org/formation/SecurityConfiguration.java
@@ -2,7 +2,9 @@ package org.formation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,29 +17,37 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http.authorizeHttpRequests().anyRequest().authenticated()
-	      .and()
-	      .formLogin()
-	      .and()
-	      .sessionManagement().maximumSessions(2)
-	      .and().and()
-	      .logout()
-	      .invalidateHttpSession(true)
-	      .logoutSuccessUrl("http://www.plb.fr");
-	    
-	    return http.build();
+		http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/fournisseurs*").hasRole("MANAGER")
+					.requestMatchers("/produits*").hasAnyRole("PRODUCT_MANAGER", "MANAGER")
+					.requestMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs/**").permitAll()
+					.requestMatchers("/api/*").permitAll()
+					.requestMatchers("/actuator/**").permitAll()
+					.anyRequest().authenticated();
+		})
+			.formLogin(Customizer.withDefaults())
+				.sessionManagement(sm -> sm.maximumSessions(2))
+				.logout(lo -> lo.invalidateHttpSession(true).logoutSuccessUrl("http://www.plb.fr"));
+
+
+		return http.build();
 	}
-	
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/resources/**", "/publics/**","/webjars/*");
+	}
+
 	@Bean
 	public SessionRegistry sessionRegistry() {
-		    return new SessionRegistryImpl();
+		return new SessionRegistryImpl();
 	}
-	
+
 	@Bean
 	public HttpSessionEventPublisher httpSessionEventPublisher() {
-	    return new HttpSessionEventPublisher();
+		return new HttpSessionEventPublisher();
 	}
-	
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
