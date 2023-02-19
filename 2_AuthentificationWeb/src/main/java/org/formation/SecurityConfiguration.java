47f0bc82cebcf155556b1df9625430443497cbd9
David THIBAU <david.thibau@gmail.com>
2023-02-19 14:13:19 +0100

    JWT Filter

--- 2_AuthentificationWeb/src/main/java/org/formation/SecurityConfiguration.java
+++ 2_AuthentificationWeb/src/main/java/org/formation/SecurityConfiguration.java
@@ -2,28 +2,51 @@ package org.formation;
 
 import java.util.Locale;
 
+import org.formation.jwt.JWTFilter;
+import org.formation.jwt.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
 import org.springframework.security.config.Customizer;
-=======
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
->>>>>>> 7d6c542 (Thymeleaf et localisation)
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
+	@Autowired
+	TokenProvider tokenProvider;
+	
+	@Bean
+	@Order(1)
+	public SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher(new AntPathRequestMatcher("/api/**"))
+			.authorizeHttpRequests(auth -> auth.requestMatchers("/api/authenticate").permitAll()
+					.anyRequest().authenticated())
+			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		        .addFilterBefore(new JWTFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+		        .csrf(csrf -> csrf.disable());
+
+
+		return http.build();
+	}
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> {
@@ -39,9 +62,9 @@ public class SecurityConfiguration {
 				.logout(lo -> lo.invalidateHttpSession(true).logoutSuccessUrl("http://www.plb.fr"))
 				.csrf(csrf -> csrf.disable());
 
+
 		return http.build();
 	}
-
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers("/resources/**", "/publics/**","/webjars/*");
@@ -61,7 +84,10 @@ public class SecurityConfiguration {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	    return authenticationConfiguration.getAuthenticationManager();
+	}
 	@Bean
 	public MessageSource messageSource() {
 		Locale.setDefault(Locale.FRENCH);
